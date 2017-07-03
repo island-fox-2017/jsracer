adc1a204d21d94dee2a1f21aabe0a429324978db
gananggww <gananggww@gmail.com>
2017-07-03 09:23:49 +0700

    Belum Finish

--- dice.js
+++ dice.js
@@ -2,11 +2,17 @@
 
 class Dice {
   constructor() {
+    this.random=0;
 
   }
   roll() {
-
+    this.random = Math.floor(Math.random()*6)+1;
+    return this.random;
   }
 }
+
+var rollRandom = new Dice()
+
+console.log(rollRandom.roll());
 
 export default Dice
--- js_racer.js
+++ js_racer.js
@@ -4,26 +4,74 @@ import Dice from "./dice.js"
 
 class JSRacer {
   constructor(players, length) {
-
+    this.player = players;
+    // this.line = (" | ");
+    this.board = [];
+    this.length = length;
+    this.pos = 0;
   }
-  print_board() {
 
-  }
   print_line(player, pos) {
-
+    var track = [];
+    for(let i = 0; i<this.length; i++){
+      // this.track.push(this.line)
+      if(i == pos){
+        track.push(player)
+      }
+      else{
+        track.push("_")
+      }
+    }
+    return track.join("|")
   }
-  advanced_player(player) {
 
-  }
-  finished() {
 
+  pemain(){
+    this.board = []
+    for (var i = 0; i < this.player; i++) {
+     var nama = "Player"+i;
+     var garis = this.print_line(nama,this.pos);
+     this.pos+=dadu.roll()
+     this.board.push(garis)
   }
-  winner() {
+  return this.board;
+}
 
-  }
-  reset_board() {
-    console.log("\x1B[2J")
+
+reset_board() {
+  console.log("\x1B[2J")
+}
+
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+
+      break;
+    }
   }
 }
+
+// sleep(1000){
+//
+// }
+
+
+let tampilkan = new JSRacer (3, 40)
+let dadu = new Dice ()
+
+
+for (var i = 0; i < 5; i++) {
+  sleep(1000);
+  tampilkan.reset_board();
+  console.log(tampilkan.pemain());
+}
+
+
+//console.log(tampilkan.print_board());
+// console.log(tampilkan.print_line());
+
 
 export default JSRacer
